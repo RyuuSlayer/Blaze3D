55b60db7a1c1c52bcbc539fd1be57cbe99a9947c
RyuuSlayer <markusm1912>
2020-08-20 22:41:17 +0200

    Modified postbuild command for Blaze project.

--- premake5.lua
+++ premake5.lua
@@ -72,7 +72,7 @@ project "Blaze"
 
 		postbuildcommands
 		{
-			("{COPY} %{cfg.buildtarget.relpath} ..\\bin\\" .. outputdir .. "\\Sandbox")
+			("{COPY} %{cfg.buildtarget.relpath} \"../bin/" .. outputdir .. "/Sandbox/\"")
 		}
 
 	filter "configurations:Debug"

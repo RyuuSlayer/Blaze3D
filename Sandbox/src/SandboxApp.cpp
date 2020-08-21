18c8abb768a7ca76cc8741dbe96b6a9ec870bf2a
RyuuSlayer <markusm1912>
2020-08-21 03:44:02 +0200

    Moved OpenGL shader implementation to OpenGLShader class + added more uniform functions.

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,8 +1,11 @@
 #include <Blaze.h>
 
+#include "Platform/OpenGL/OpenGLShader.h"
+
 #include "imgui/imgui.h"
 
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 class ExampleLayer : public Blaze::Layer
 {
@@ -89,9 +92,9 @@ public:
 			}
 		)";
 
-		m_Shader.reset(new Blaze::Shader(vertexSrc, fragmentSrc));
+		m_Shader.reset(Blaze::Shader::Create(vertexSrc, fragmentSrc));
 
-		std::string blueShaderVertexSrc = R"(
+		std::string flatColorShaderVertexSrc = R"(
 			#version 330 core
 			
 			layout(location = 0) in vec3 a_Position;
@@ -108,20 +111,22 @@ public:
 			}
 		)";
 
-		std::string blueShaderFragmentSrc = R"(
+		std::string flatColorShaderFragmentSrc = R"(
 			#version 330 core
 			
 			layout(location = 0) out vec4 color;
 
 			in vec3 v_Position;
 
+			uniform vec3 u_Color;
+
 			void main()
 			{
-				color = vec4(0.2, 0.3, 0.8, 1.0);
+				color = vec4(u_Color, 1.0);
 			}
 		)";
 
-		m_BlueShader.reset(new Blaze::Shader(blueShaderVertexSrc, blueShaderFragmentSrc));
+		m_FlatColorShader.reset(Blaze::Shader::Create(flatColorShaderVertexSrc, flatColorShaderFragmentSrc));
 	}
 
 	void OnUpdate(Blaze::Timestep ts) override
@@ -151,13 +156,16 @@ public:
 
 		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
 
+		std::dynamic_pointer_cast<Blaze::OpenGLShader>(m_FlatColorShader)->Bind();
+		std::dynamic_pointer_cast<Blaze::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat3("u_Color", m_SquareColor);
+
 		for (int y = 0; y < 20; y++)
 		{
 			for (int x = 0; x < 20; x++)
 			{
 				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
 				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
-				Blaze::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+				Blaze::Renderer::Submit(m_FlatColorShader, m_SquareVA, transform);
 			}
 		}
 
@@ -168,6 +176,9 @@ public:
 
 	virtual void OnImGuiRender() override
 	{
+		ImGui::Begin("Settings");
+		ImGui::ColorEdit3("Square Color", glm::value_ptr(m_SquareColor));
+		ImGui::End();
 	}
 
 	void OnEvent(Blaze::Event& event) override
@@ -178,7 +189,7 @@ public:
 		std::shared_ptr<Blaze::Shader> m_Shader;
 		std::shared_ptr<Blaze::VertexArray> m_VertexArray;
 
-		std::shared_ptr<Blaze::Shader> m_BlueShader;
+		std::shared_ptr<Blaze::Shader> m_FlatColorShader;
 		std::shared_ptr<Blaze::VertexArray> m_SquareVA;
 
 		Blaze::OrthographicCamera m_Camera;
@@ -187,6 +198,8 @@ public:
 
 		float m_CameraRotation = 0.0f;
 		float m_CameraRotationSpeed = 180.0f;
+
+		glm::vec3 m_SquareColor = { 0.2f, 0.3f, 0.8f };
 };
 
 class Sandbox : public Blaze::Application

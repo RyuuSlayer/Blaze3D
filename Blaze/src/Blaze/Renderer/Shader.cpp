15cdddcd6c6e352ba751dbfee00f43babaf3f52a
RyuuSlayer <markusm1912>
2020-08-21 04:42:32 +0200

    Shaders can be read from files now.

--- Blaze/src/Blaze/Renderer/Shader.cpp
+++ Blaze/src/Blaze/Renderer/Shader.cpp
@@ -6,6 +6,18 @@
 
 namespace Blaze {
 
+	Shader* Shader::Create(const std::string& filepath)
+	{
+		switch (Renderer::GetAPI())
+		{
+		case RendererAPI::API::None:    BZ_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
+		case RendererAPI::API::OpenGL:  return new OpenGLShader(filepath);
+		}
+
+		BZ_CORE_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
+	}
+
 	Shader* Shader::Create(const std::string& vertexSrc, const std::string& fragmentSrc)
 	{
 		switch (Renderer::GetAPI())
--- premake5.lua
+++ premake5.lua
@@ -9,6 +9,11 @@ workspace "Blaze"
 		"Dist"
 	}
 
+	flags
+	{
+		"MultiProcessorCompile"
+	}
+
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
 -- Include directories relative to root folder (solution directory)

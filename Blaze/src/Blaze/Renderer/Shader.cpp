0a329d63b8be8f2250f3f36b9305042e19012d02
RyuuSlayer <markusm1912>
2020-08-21 04:56:46 +0200

    Added ShaderLibrary + misc fixes.

--- Blaze/src/Blaze/Renderer/Shader.cpp
+++ Blaze/src/Blaze/Renderer/Shader.cpp
@@ -6,28 +6,65 @@
 
 namespace Blaze {
 
-	Shader* Shader::Create(const std::string& filepath)
+	Ref<Shader> Shader::Create(const std::string& filepath)
 	{
 		switch (Renderer::GetAPI())
 		{
 		case RendererAPI::API::None:    BZ_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
-		case RendererAPI::API::OpenGL:  return new OpenGLShader(filepath);
+		case RendererAPI::API::OpenGL:  return std::make_shared<OpenGLShader>(filepath);
 		}
 
 		BZ_CORE_ASSERT(false, "Unknown RendererAPI!");
 		return nullptr;
 	}
 
-	Shader* Shader::Create(const std::string& vertexSrc, const std::string& fragmentSrc)
+	Ref<Shader> Shader::Create(const std::string& name, const std::string& vertexSrc, const std::string& fragmentSrc)
 	{
 		switch (Renderer::GetAPI())
 		{
-			case RendererAPI::API::None:    BZ_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
-			case RendererAPI::API::OpenGL:  return new OpenGLShader(vertexSrc, fragmentSrc);
+		case RendererAPI::API::None:    BZ_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
+		case RendererAPI::API::OpenGL:  return std::make_shared<OpenGLShader>(name, vertexSrc, fragmentSrc);
 		}
 
 		BZ_CORE_ASSERT(false, "Unknown RendererAPI!");
 		return nullptr;
+	}
+
+	void ShaderLibrary::Add(const std::string& name, const Ref<Shader>& shader)
+	{
+		BZ_CORE_ASSERT(!Exists(name), "Shader already exists!");
+		m_Shaders[name] = shader;
+	}
+
+	void ShaderLibrary::Add(const Ref<Shader>& shader)
+	{
+		auto& name = shader->GetName();
+		Add(name, shader);
+	}
+
+	Blaze::Ref<Blaze::Shader> ShaderLibrary::Load(const std::string& filepath)
+	{
+		auto shader = Shader::Create(filepath);
+		Add(shader);
+		return shader;
+	}
+
+	Blaze::Ref<Blaze::Shader> ShaderLibrary::Load(const std::string& name, const std::string& filepath)
+	{
+		auto shader = Shader::Create(filepath);
+		Add(name, shader);
+		return shader;
+	}
+
+	Blaze::Ref<Blaze::Shader> ShaderLibrary::Get(const std::string& name)
+	{
+		BZ_CORE_ASSERT(Exists(name), "Shader not found!");
+		return m_Shaders[name];
+	}
+
+	bool ShaderLibrary::Exists(const std::string& name) const
+	{
+		return m_Shaders.find(name) != m_Shaders.end();
 	}
 
 }
--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -87,7 +87,7 @@ public:
 			}
 		)";
 
-		m_Shader.reset(Blaze::Shader::Create(vertexSrc, fragmentSrc));
+		m_Shader = Blaze::Shader::Create("VertexPosColor", vertexSrc, fragmentSrc);
 
 		std::string flatColorShaderVertexSrc = R"(
 			#version 330 core
@@ -116,42 +116,15 @@ public:
 			}
 		)";
 
-		m_FlatColorShader.reset(Blaze::Shader::Create(flatColorShaderVertexSrc, flatColorShaderFragmentSrc));
+		m_FlatColorShader = Blaze::Shader::Create("FlatColor", flatColorShaderVertexSrc, flatColorShaderFragmentSrc);
 
-		std::string textureShaderVertexSrc = R"(
-			#version 330 core
-			
-			layout(location = 0) in vec3 a_Position;
-			layout(location = 1) in vec2 a_TexCoord;
-			uniform mat4 u_ViewProjection;
-			uniform mat4 u_Transform;
-			out vec2 v_TexCoord;
-			void main()
-			{
-				v_TexCoord = a_TexCoord;
-				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);	
-			}
-		)";
-
-		std::string textureShaderFragmentSrc = R"(
-			#version 330 core
-			
-			layout(location = 0) out vec4 color;
-			in vec2 v_TexCoord;
-			
-			uniform sampler2D u_Texture;
-			void main()
-			{
-				color = texture(u_Texture, v_TexCoord);
-			}
-		)";
-
-		m_TextureShader.reset(Blaze::Shader::Create(textureShaderVertexSrc, textureShaderFragmentSrc));
+		auto textureShader = m_ShaderLibrary.Load("assets/shaders/Texture.glsl");
 
 		m_Texture = Blaze::Texture2D::Create("assets/textures/Checkerboard.png");
+		m_ChernoLogoTexture = Blaze::Texture2D::Create("assets/textures/ChernoLogo.png");
 
-		std::dynamic_pointer_cast<Blaze::OpenGLShader>(m_TextureShader)->Bind();
-		std::dynamic_pointer_cast<Blaze::OpenGLShader>(m_TextureShader)->UploadUniformInt("u_Texture", 0);
+		std::dynamic_pointer_cast<Blaze::OpenGLShader>(textureShader)->Bind();
+		std::dynamic_pointer_cast<Blaze::OpenGLShader>(textureShader)->UploadUniformInt("u_Texture", 0);
 	}
 
 	void OnUpdate(Blaze::Timestep ts) override
@@ -194,8 +167,12 @@ public:
 			}
 		}
 
+		auto textureShader = m_ShaderLibrary.Get("Texture");
+
 		m_Texture->Bind();
-		Blaze::Renderer::Submit(m_TextureShader, m_SquareVA, glm::scale(glm::mat4(1.0f), glm::vec3(1.5f)));
+		Blaze::Renderer::Submit(textureShader, m_SquareVA, glm::scale(glm::mat4(1.0f), glm::vec3(1.5f)));
+		m_ChernoLogoTexture->Bind();
+		Blaze::Renderer::Submit(textureShader, m_SquareVA, glm::scale(glm::mat4(1.0f), glm::vec3(1.5f)));
 
 		// Triangle
 		// Blaze::Renderer::Submit(m_Shader, m_VertexArray);
@@ -214,13 +191,14 @@ public:
 	{
 	}
 private:
+	Blaze::ShaderLibrary m_ShaderLibrary;
 	Blaze::Ref<Blaze::Shader> m_Shader;
 	Blaze::Ref<Blaze::VertexArray> m_VertexArray;
 
-	Blaze::Ref<Blaze::Shader> m_FlatColorShader, m_TextureShader;
+	Blaze::Ref<Blaze::Shader> m_FlatColorShader;
 	Blaze::Ref<Blaze::VertexArray> m_SquareVA;
 
-	Blaze::Ref<Blaze::Texture2D> m_Texture;
+	Blaze::Ref<Blaze::Texture2D> m_Texture, m_ChernoLogoTexture;
 
 	Blaze::OrthographicCamera m_Camera;
 	glm::vec3 m_CameraPosition;

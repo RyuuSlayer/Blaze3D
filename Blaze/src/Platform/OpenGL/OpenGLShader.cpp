9749b462ff24d49a5f10176ec74dbb00ef31edb3
RyuuSlayer <markusm1912>
2020-08-22 22:53:18 +0200

    Added textures to batch renderer.

--- Blaze/src/Platform/OpenGL/OpenGLShader.cpp
+++ Blaze/src/Platform/OpenGL/OpenGLShader.cpp
@@ -207,6 +207,13 @@ namespace Blaze {
 		UploadUniformInt(name, value);
 	}
 
+	void OpenGLShader::SetIntArray(const std::string& name, int* values, uint32_t count)
+	{
+		BZ_PROFILE_FUNCTION();
+
+		UploadUniformIntArray(name, values, count);
+	}
+
 	void OpenGLShader::SetFloat(const std::string& name, float value)
 	{
 		BZ_PROFILE_FUNCTION();
@@ -239,6 +246,12 @@ namespace Blaze {
 	{
 		GLint location = glGetUniformLocation(m_RendererID, name.c_str());
 		glUniform1i(location, value);
+	}
+
+	void OpenGLShader::UploadUniformIntArray(const std::string& name, int* values, uint32_t count)
+	{
+		GLint location = glGetUniformLocation(m_RendererID, name.c_str());
+		glUniform1iv(location, count, values);
 	}
 
 	void OpenGLShader::UploadUniformFloat(const std::string& name, float value)

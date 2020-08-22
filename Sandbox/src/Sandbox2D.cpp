9791060ad677341350439d8bd90fda8cfcc94e55
RyuuSlayer <markusm1912>
2020-08-22 16:11:01 +0200

    Updated Sandbox2D.

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -38,9 +38,9 @@ void Sandbox2D::OnUpdate(Blaze::Timestep ts)
 	{
 		BZ_PROFILE_SCOPE("Renderer Draw");
 		Blaze::Renderer2D::BeginScene(m_CameraController.GetCamera());
-		Blaze::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
+		Blaze::Renderer2D::DrawRotatedQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, glm::radians(-45.0f), { 0.8f, 0.2f, 0.3f, 1.0f });
 		Blaze::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, { 0.2f, 0.3f, 0.8f, 1.0f });
-		Blaze::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture);
+		Blaze::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture, 10.0f);
 		Blaze::Renderer2D::EndScene();
 	}
 }

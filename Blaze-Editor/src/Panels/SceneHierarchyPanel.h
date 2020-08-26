08d7b3ed22fb06f6b5005e71d78bd3d79ce598c7
RyuuM3gum1n <maurermarkuss6@gmail.com>
2020-08-27 00:41:15 +0200

    Added Scene Hierarchy panel.

---
+++ Blaze-Editor/src/Panels/SceneHierarchyPanel.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "Blaze/Core/Base.h"
+#include "Blaze/Core/Log.h"
+#include "Blaze/Scene/Scene.h"
+#include "Blaze/Scene/Entity.h"
+
+namespace Blaze {
+
+	class SceneHierarchyPanel
+	{
+	public:
+		SceneHierarchyPanel() = default;
+		SceneHierarchyPanel(const Ref<Scene>& scene);
+
+		void SetContext(const Ref<Scene>& scene);
+
+		void OnImGuiRender();
+	private:
+		void DrawEntityNode(Entity entity);
+	private:
+		Ref<Scene> m_Context;
+		Entity m_SelectionContext;
+	};
+
+}

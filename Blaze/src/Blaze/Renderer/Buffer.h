d08891d73d5ea755525178e84d60a3e9b405f890
RyuuM3gum1n <maurermarkuss6@gmail.com>
2020-10-19 07:18:57 +0200

    Transform component now stores translation rotation scale instead of …

--- Blaze/src/Blaze/Renderer/Buffer.h
+++ Blaze/src/Blaze/Renderer/Buffer.h
@@ -70,7 +70,7 @@ namespace Blaze {
 	public:
 		BufferLayout() {}
 
-		BufferLayout(const std::initializer_list<BufferElement>& elements)
+		BufferLayout(std::initializer_list<BufferElement> elements)
 			: m_Elements(elements)
 		{
 			CalculateOffsetsAndStride();
--- Blaze/src/Blaze/Scene/Scene.cpp
+++ Blaze/src/Blaze/Scene/Scene.cpp
@@ -38,7 +38,6 @@ namespace Blaze {
 					{
 						nsc.Instance = nsc.InstantiateScript();
 						nsc.Instance->m_Entity = Entity{ entity, this };
-
 						nsc.Instance->OnCreate();
 					}
 
@@ -48,7 +47,7 @@ namespace Blaze {
 
 		// Render 2D
 		Camera* mainCamera = nullptr;
-		glm::mat4* cameraTransform = nullptr;
+		glm::mat4 cameraTransform;
 		{
 			auto view = m_Registry.view<TransformComponent, CameraComponent>();
 			for (auto entity : view)
@@ -58,7 +57,7 @@ namespace Blaze {
 				if (camera.Primary)
 				{
 					mainCamera = &camera.Camera;
-					cameraTransform = &transform.Transform;
+					cameraTransform = transform.GetTransform();
 					break;
 				}
 			}
@@ -66,14 +65,14 @@ namespace Blaze {
 
 		if (mainCamera)
 		{
-			Renderer2D::BeginScene(*mainCamera, *cameraTransform);
+			Renderer2D::BeginScene(*mainCamera, cameraTransform);
 
 			auto group = m_Registry.group<TransformComponent>(entt::get<SpriteRendererComponent>);
 			for (auto entity : group)
 			{
 				auto [transform, sprite] = group.get<TransformComponent, SpriteRendererComponent>(entity);
 
-				Renderer2D::DrawQuad(transform, sprite.Color);
+				Renderer2D::DrawQuad(transform.GetTransform(), sprite.Color);
 			}
 
 			Renderer2D::EndScene();

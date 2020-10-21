fc667606f9af6b016d6e5ad6139aaf4cde282d56
RyuuM3gum1n <maurermarkuss6@gmail.com>
2020-10-21 11:22:35 +0200

    Added UI to add/remove entities and components + fixed bug when added during runtime.

--- Blaze/src/Blaze/Scene/Scene.cpp
+++ Blaze/src/Blaze/Scene/Scene.cpp
@@ -1,3 +1,4 @@
+
 #include "bzpch.h"
 #include "Scene.h"
 
@@ -25,6 +26,11 @@ namespace Blaze {
 		auto& tag = entity.AddComponent<TagComponent>();
 		tag.Tag = name.empty() ? "Entity" : name;
 		return entity;
+	}
+
+	void Scene::DestroyEntity(Entity entity)
+	{
+		m_Registry.destroy(entity);
 	}
 
 	void Scene::OnUpdate(Timestep ts)
@@ -94,6 +100,38 @@ namespace Blaze {
 				cameraComponent.Camera.SetViewportSize(width, height);
 		}
 
+	}
+
+	template<typename T>
+	void Scene::OnComponentAdded(Entity entity, T& component)
+	{
+		static_assert(false);
+	}
+
+	template<>
+	void Scene::OnComponentAdded<TransformComponent>(Entity entity, TransformComponent& component)
+	{
+	}
+
+	template<>
+	void Scene::OnComponentAdded<CameraComponent>(Entity entity, CameraComponent& component)
+	{
+		component.Camera.SetViewportSize(m_ViewportWidth, m_ViewportHeight);
+	}
+
+	template<>
+	void Scene::OnComponentAdded<SpriteRendererComponent>(Entity entity, SpriteRendererComponent& component)
+	{
+	}
+
+	template<>
+	void Scene::OnComponentAdded<TagComponent>(Entity entity, TagComponent& component)
+	{
+	}
+
+	template<>
+	void Scene::OnComponentAdded<NativeScriptComponent>(Entity entity, NativeScriptComponent& component)
+	{
 	}
 
 }
--- Blaze/src/Blaze/Scene/Scene.h
+++ Blaze/src/Blaze/Scene/Scene.h
@@ -2,6 +2,7 @@
 
 #include "entt.hpp"
 
+
 #include "Blaze/Core/Timestep.h"
 
 namespace Blaze {
@@ -15,9 +16,13 @@ namespace Blaze {
 		~Scene();
 
 		Entity CreateEntity(const std::string& name = std::string());
+		void DestroyEntity(Entity entity);
 
 		void OnUpdate(Timestep ts);
 		void OnViewportResize(uint32_t width, uint32_t height);
+	private:
+		template<typename T>
+		void OnComponentAdded(Entity entity, T& component);
 	private:
 		entt::registry m_Registry;
 		uint32_t m_ViewportWidth = 0, m_ViewportHeight = 0;

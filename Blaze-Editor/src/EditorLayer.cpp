157b812e13fa15dff8577124bef00c3e4ac11949
RyuuM3gum1n <33106127+RyuuSlayer@users.noreply.github.com>
2021-02-11 15:10:04 +0100

    Fixes in ImGuizmo and OpenGLVertexArray.
    Fixed ImGuizmo crash when changing type while operating it, fixed OpenGLVertexArray for integer attributes, added missing "SetFloat2" in Renderer for completeness.

--- Blaze-Editor/src/EditorLayer.cpp
+++ Blaze-Editor/src/EditorLayer.cpp
@@ -359,17 +359,29 @@ namespace Blaze {
 
 		// Gizmos
 		case Key::Q:
-			m_GizmoType = -1;
+		{
+			if (!ImGuizmo::IsUsing())
+				m_GizmoType = -1;
 			break;
+		}
 		case Key::W:
-			m_GizmoType = ImGuizmo::OPERATION::TRANSLATE;
+		{
+			if (!ImGuizmo::IsUsing())
+				m_GizmoType = ImGuizmo::OPERATION::TRANSLATE;
 			break;
+		}
 		case Key::E:
-			m_GizmoType = ImGuizmo::OPERATION::ROTATE;
+		{
+			if (!ImGuizmo::IsUsing())
+				m_GizmoType = ImGuizmo::OPERATION::ROTATE;
 			break;
+		}
 		case Key::R:
-			m_GizmoType = ImGuizmo::OPERATION::SCALE;
+		{
+			if (!ImGuizmo::IsUsing())
+				m_GizmoType = ImGuizmo::OPERATION::SCALE;
 			break;
+		}
 		}
 	}
 
--- Blaze/src/Platform/OpenGL/OpenGLShader.cpp
+++ Blaze/src/Platform/OpenGL/OpenGLShader.cpp
@@ -218,6 +218,14 @@ namespace Blaze {
 		UploadUniformFloat(name, value);
 	}
 
+	void OpenGLShader::SetFloat2(const std::string& name, const glm::vec2& value)
+	{
+		BZ_PROFILE_FUNCTION();
+
+		UploadUniformFloat2(name, value);
+	}
+
+
 	void OpenGLShader::SetFloat3(const std::string& name, const glm::vec3& value)
 	{
 		BZ_PROFILE_FUNCTION();
